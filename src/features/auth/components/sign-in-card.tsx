f4298382ea7019892016febf37fe0ae10d3d6969
Ulrich Dinh <duong0028@gmail.com>
2025-02-02 19:52:37 +0200

    feat: add login+signup page.

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -26,8 +26,8 @@ export const SignInCard = () => {
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
-      email: "",
-      password: "",
+      email: "g@gmail.com",
+      password: "123",
     },
   });
 
