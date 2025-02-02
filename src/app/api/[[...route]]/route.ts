33e86092c330ba4b87885f1dd0bea6ac05af5651
Ulrich Dinh <duong0028@gmail.com>
2025-02-02 20:13:40 +0200

    fix: update routes constant

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -4,9 +4,11 @@ import auth from "@/features/auth/server/route";
 
 const app = new Hono().basePath("/api")
 
-export const routes = app.route('/auth', auth)
+//eslint-disable-next-line
+const routes = app.route('/auth', auth)
 
 export const GET = handle(app)
 export const POST = handle(app)
 
-export type AppType = typeof routes 
+export type AppType = typeof routes
+
