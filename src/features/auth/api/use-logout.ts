8f0bce79d94f85cd6352bf11be971fec48045894
Ulrich Dinh <duong0028@gmail.com>
2025-02-04 01:17:40 +0200

    feat: create logout hooks that invalidate current user data

---
+++ src/features/auth/api/use-logout.ts
@@ -0,0 +1,29 @@
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
+
+type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
+
+export const useLogout = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const response = await client.api.auth.logout.$post();
+      return await response.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["current"] }); // trigger a refetch of the current user data
+    },
+    onError: () => {
+      toast.error("Failed to log out.");
+    },
+  });
+
+  return mutation;
+};
