12ed1b29ce830ab9f5c9bea8365f8fedc728d0a7
Ulrich Dinh <duong0028@gmail.com>
2025-02-05 01:12:00 +0200

    feat: connect workspace to main page + add ws schema

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import { getCurrent } from "@/features/auth/action";
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -9,7 +10,7 @@ export default async function Home() {
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <CreateWorkspaceForm />
     </div>
   )
 }
