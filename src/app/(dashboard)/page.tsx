838643f44e845bd387a37728da815a3561438478
_ulriich_ <duong0028@gmail.com>
2025-02-04 22:53:53 +0200

    Feat/dashboard (#6)
    * feat: navbar
    
    * feat: navigation route
    
    * feat: dashboard layout
    
    * feat: sidebar
    
    * feat: add mobile sidebar view
    
    * feat: add mobile navbar view
    
    * fix: fix title and name

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,15 @@
 import { getCurrent } from "@/features/auth/action";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
-  if (!user) {
-    return redirect("/sign-in");
-  }
+
+  if (!user) redirect("/sign-in");
+
+
   return (
-    <div className="flex gap-4">
-      <UserButton />
+    <div>
+      <h1>Dashboard</h1>
     </div>
-  );
+  )
 }
