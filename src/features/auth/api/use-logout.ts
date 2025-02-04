6ef494e4c434bdcdb0858599c67c4acbc35505a7
Ulrich Dinh <duong0028@gmail.com>
2025-02-04 15:09:55 +0200

    feat: invalidate query in signin, register page + reload

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -4,18 +4,20 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
-
+  const router = useRouter()
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.logout.$post();
       return await response.json();
     },
     onSuccess: () => {
+      router.refresh()
       queryClient.invalidateQueries({ queryKey: ["current"] }); // trigger a refetch of the current user data
     },
     onError: () => {
