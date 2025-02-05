00e2e956a93088993ff8e5f01b5a31aeefa1a12f
Ulrich Dinh <duong0028@gmail.com>
2025-02-06 00:21:49 +0200

    feat: create member for user creates ws

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,8 +2,9 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
-import { DATABASE_ID, WORKSPACES_ID, IMAGES_BUCKET_ID } from "@/config";
+import { DATABASE_ID, WORKSPACES_ID, IMAGES_BUCKET_ID, MEMBERS_ID } from "@/config";
 import { ID } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
@@ -52,11 +53,18 @@ const app = new Hono()
           name,
           userId: user.$id,
           imageUrl: uploadedImageUrl,
+          // inviteCode: generateInviteCode(6),
         }
       );
 
+      await databases.createDocument(DATABASE_ID, MEMBERS_ID, ID.unique(), {
+        userId: user.$id,
+        workspaceId: workspace.$id,
+        role: MemberRole.ADMIN,
+      });
+
       return c.json({ data: workspace });
-    },
+    }
   )
 
 export default app;
