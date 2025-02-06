c7f64ff422e13ac6566cb5250fc6c15d16405948
Ulrich Dinh <duong0028@gmail.com>
2025-02-07 00:57:05 +0200

    fix: fix 'force-dynamic' error

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,6 +3,8 @@ import { SignInCard } from '@/features/auth/components/sign-in-card'
 import { redirect } from 'next/navigation';
 import React from 'react'
 
+export const dynamic = 'force-dynamic'; // Add this line
+
 const Signin = async () => {
   const user = await getCurrent();
 
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import { getCurrent } from "@/features/auth/action";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 import { redirect } from "next/navigation";
 
+export const dynamic = 'force-dynamic'; // Add this line
+
 export default async function Home() {
   const user = await getCurrent();
 
