f1c9d9297683df1074b9b2fbc15a7d61883714fe
Ulrich Dinh <duong0028@gmail.com>
2025-02-10 02:25:05 +0200

    feat: add update workspace hook

--- src/features/workspaces/hooks/use-get-workspace-id.ts
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'next/navigation';
-
-export const useGetWorkspaceId = () => {
-  const params = useParams();
-  const workspaceId = params.workspaceId;
-
-  return Array.isArray(workspaceId) ? workspaceId[0] : workspaceId;
-};
