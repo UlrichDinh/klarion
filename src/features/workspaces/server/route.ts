9ccf8103f80d2eedb8cd9ccccc11a6ebd71d9262
Ulrich Dinh <duong0028@gmail.com>
2025-02-12 00:17:29 +0200

    feat: add reset invi code route

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -164,10 +164,36 @@ const app = new Hono()
       await databases.deleteDocument(DATABASE_ID, WORKSPACES_ID, workspaceId);
 
       return c.json({ data: { $id: workspaceId } }, 200);
-    } catch (error) {
-      console.error('Delete Error:', error);
+    } catch {
       return c.json({ error: 'Internal Server Error' }, 500);
     }
+  })
+  .post('/:workspaceId/reset-invite-code', sessionMiddleware, async (c) => {
+    const databases = c.get('databases');
+    const user = c.get('user');
+
+    const { workspaceId } = c.req.param();
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member || member.role !== MemberRole.ADMIN) {
+      return c.json({ error: 'Unauthorized' }, 401);
+    }
+
+    const workspace = await databases.updateDocument(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId,
+      {
+        inviteCode: generateInviteCode(6),
+      }
+    );
+
+    return c.json({ data: workspace });
   });
 
 export default app;
