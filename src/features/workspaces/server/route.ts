0b5d6ca4e0e60352e5102dfeabbac80849ae299f
Ulrich Dinh <duong0028@gmail.com>
2025-02-13 01:12:35 +0200

    feat: add workspaceId/info route

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -236,6 +236,24 @@ const app = new Hono()
 
       return c.json({ data: workspace });
     }
-  );
+  )
+  .get('/:workspaceId/info', sessionMiddleware, async (c) => {
+    const databases = c.get('databases');
+    const { workspaceId } = c.req.param();
+
+    const workspace = await databases.getDocument<WorkspaceType>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId
+    );
+
+    return c.json({
+      data: {
+        $id: workspace.$id,
+        name: workspace.name,
+        imageUrl: workspace.imageUrl,
+      },
+    });
+  });
 
 export default app;
