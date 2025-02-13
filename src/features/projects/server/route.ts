751a325c2013c97912d934960c1172b77601c002
Ulrich Dinh <duong0028@gmail.com>
2025-02-13 21:04:41 +0200

    feat: add route create project

---
+++ src/features/projects/server/route.ts
@@ -0,0 +1,67 @@
+import { zValidator } from '@hono/zod-validator';
+import { Hono } from 'hono';
+import { ID } from 'node-appwrite';
+
+import { getMember } from '@/features/members/utils';
+
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from '@/config';
+import { sessionMiddleware } from '@/lib/session-middleware';
+
+import { createProjectSchema } from '../schemas';
+
+const app = new Hono().post(
+  '/',
+  sessionMiddleware,
+  zValidator('form', createProjectSchema),
+  async (c) => {
+    const databases = c.get('databases');
+    const storage = c.get('storage');
+    const user = c.get('user');
+
+    const { name, image, workspaceId } = c.req.valid('form');
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      return c.json({ error: 'Unauthorized' }, 401);
+    }
+
+    let uploadedImageUrl: string | undefined;
+
+    if (image instanceof File) {
+      const file = await storage.createFile(
+        IMAGES_BUCKET_ID,
+        ID.unique(),
+        image
+      );
+
+      const arrayBuffer = await storage.getFilePreview(
+        IMAGES_BUCKET_ID,
+        file.$id
+      );
+
+      uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+        arrayBuffer
+      ).toString('base64')}`;
+    }
+
+    const project = await databases.createDocument(
+      DATABASE_ID,
+      PROJECTS_ID,
+      ID.unique(),
+      {
+        name,
+        imageUrl: uploadedImageUrl,
+        workspaceId,
+      }
+    );
+
+    return c.json({ data: project });
+  }
+);
+
+export default app;
