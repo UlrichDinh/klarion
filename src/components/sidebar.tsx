afd5aa823d664b11c85b2ef7a11a3ca1afd8cfe8
Ulrich Dinh <duong0028@gmail.com>
2025-02-13 21:08:04 +0200

    feat: add project component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { DottedSeparator } from './dotted-separator';
 import { Navigation } from './navigation';
 import { WorkspaceSwitcher } from '@/features/auth/components/workspace-switcher';
+import { Projects } from './projects';
 
 export const Sidebar = () => {
   return (
@@ -19,6 +20,7 @@ export const Sidebar = () => {
       <DottedSeparator className="my-4" />
       <Navigation />
       <DottedSeparator className="my-4" />
+      <Projects />
     </aside>
   );
 };
