3b5aaaf59a68d3c919ce67fb1afa7808aa9b2776
Ulrich Dinh <duong0028@gmail.com>
2025-02-13 19:14:30 +0200

    feat: add useGetMembers hook

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,11 +2,15 @@ import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import auth from '@/features/auth/server/route';
 import workspaces from '@/features/workspaces/server/route';
+import member from '@/features/members/server/route';
 
 const app = new Hono().basePath('/api');
 
 //eslint-disable-next-line
-const routes = app.route('/auth', auth).route('/workspaces', workspaces);
+const routes = app
+  .route('/auth', auth)
+  .route('/workspaces', workspaces)
+  .route('/members', member);
 
 export const GET = handle(app);
 export const POST = handle(app);
--- src/features/members/types.ts
+++ src/features/members/types.ts
@@ -1,5 +1,13 @@
+import { Models } from 'node-appwrite';
+
 /* eslint-disable no-unused-vars */
 export enum MemberRole {
   ADMIN = 'ADMIN',
   MEMBER = 'MEMBER',
 }
+
+export type Member = Models.Document & {
+  workspaceId: string;
+  userId: string;
+  role: MemberRole;
+};
