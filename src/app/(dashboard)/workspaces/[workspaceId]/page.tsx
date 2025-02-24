5595c832bda82328358c41091c87d736d4241da8
Ulrich Dinh <duong0028@gmail.com>
2025-02-24 23:32:50 +0200

    feat: add workspace analytics to WorkspaceIdPage component

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,12 +1,15 @@
-'use client';
+import { redirect } from 'next/navigation';
 
-import { useGetWorkspaceId } from '@/features/workspaces/hooks/use-get-workspace-id';
-import React from 'react';
+import { getCurrent } from '@/features/auth/queries';
 
-const WorkspaceId = () => {
-  const workspaceId = useGetWorkspaceId();
+import { WorkspaceIdClient } from './client';
 
-  return <div>{workspaceId}</div>;
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent();
+
+  if (!user) redirect('/sign-in');
+
+  return <WorkspaceIdClient />;
 };
 
-export default WorkspaceId;
+export default WorkspaceIdPage;
