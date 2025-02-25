171cfa86196070a63dfca703b1c9d87fba807df8
Ulrich Dinh <duong0028@gmail.com>
2025-02-25 13:02:49 +0200

    fix: minor fixes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar = () => {
         <Link href="/">
           <Image src="/logo.svg" alt="logo" width={50} height={39} />
         </Link>
-        <p className="font-bold text-lg">Project Management</p>
+        <p className="font-bold text-lg">Klarion</p>
       </div>
       <DottedSeparator className="my-4" />
       <WorkspaceSwitcher />
